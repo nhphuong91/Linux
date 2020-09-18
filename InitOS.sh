79cf6a2f6157d067ab6dba3288c409f2ba3abac4
nhphuong91 <nhp12345@gmail.com>
2020-09-18 18:08:16 +0700

    update initOS

--- InitOS.sh
+++ InitOS.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo add-apt-repository ppa:umang/indicator-stickynotes
 sudo apt-get update
 sudo apt-get -y install aptitude
-sudo aptitude -y install git htop cmake ibus-unikey python-pip python3-pip
+sudo aptitude -y install git htop cmake ibus-bamboo python-pip python3-pip indicator-stickynotes
 
 ibus restart

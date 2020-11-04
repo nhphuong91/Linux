54666ca394dfd8cb080059c4f0f3de8c823b240d
nhphuong <49468842+nhphuong91@users.noreply.github.com>
2020-11-04 17:15:26 +0700

    Add "exfat" init

--- InitOS.sh
+++ InitOS.sh
@@ -3,8 +3,10 @@
 sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo add-apt-repository ppa:umang/indicator-stickynotes
+sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get -y install aptitude
-sudo aptitude -y install git htop cmake ibus-bamboo python-pip python3-pip indicator-stickynotes
+sudo aptitude -y install git htop cmake ibus-bamboo python-pip python3-pip indicator-stickynotes \
+                         exfat-fuse exfat-utils
 
 ibus restart
